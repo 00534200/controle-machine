fa131bd5a87465da86c3fd42f1ed46199a691e5d
00534200 <nicoodu77@hotmail.fr>
2016-06-15 12:01:57 +0200

    Create message.php

---
+++ message.php
@@ -0,0 +1,20 @@
+<?php
+
+setcookie('lg', $_GET['remarque'], time() + 365*24*3600);
+
+if (isset($_GET['remarque'])){
+	echo $_GET['remarque'];
+}
+
+else if (isset($_GET['remarque'])){
+	echo $_GET['remarque'];
+}
+
+else if (isset($_GET['remarque'])){
+	echo $_GET['remarque'];
+}
+
+
+
+
+?>
