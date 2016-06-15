99e841ec377a46a4aa6b9c3f3b0a34a03f35a2b3
00534200 <nicoodu77@hotmail.fr>
2016-06-15 12:02:16 +0200

    Create question.php

---
+++ question.php
@@ -0,0 +1,23 @@
+<?php
+
+
+
+if (isset($_COOKIE['lg'])){
+  if($_COOKIE['lg'] == 'bonjour'){
+    echo 'comment va tu ? ';
+  }
+  else if($_COOKIE['lg'] == 'hello'){
+    echo 'how are you ? ';
+  }
+  
+   else if($_COOKIE['lg'] == 'hollz'){
+    echo '¿ Como estas ? ';
+  }
+  
+  
+}
+
+
+
+
+?>
